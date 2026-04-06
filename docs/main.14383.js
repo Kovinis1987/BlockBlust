e1e2f45ad0a3c70c7db2bf7d4bf3e87a6f917dcb
kovin <kir-predator@mail.ru>
2026-04-07 01:03:53 +0800

    fix build

--- docs/main.14383.js
+++ docs/main.14383.js
@@ -118,18 +118,18 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('docs/src/settings.7ca37.js');
-        require('docs/src/cocos2d-runtime.js');
+        require('src/settings.1c23f.js');
+        require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
-            require('docs/src/physics.js');
+            require('src/physics.js');
         }
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('docs/src/settings.7ca37.js');
-        require('docs/src/cocos2d-jsb.js');
+        require('src/settings.1c23f.js');
+        require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
-            require('docs/src/physics.js');
+            require('src/physics.js');
         }
         require('jsb-adapter/jsb-engine.js');
     }
